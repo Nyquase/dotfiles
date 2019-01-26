b9fd839920fc3fb2833fde4ded56bfb5765a9843
Adrien Rouhète <adrien.rouhete@epitech.eu>
2019-01-26 19:53:00 +0100

    Added dogelock

---
+++ dogelock/README.md
@@ -0,0 +1,6 @@
+Credits to https://github.com/maximelouet/dogelock
+This lock is Maxime's one but with a few modifications
+#Dependencies:
+- imagemagick
+- i3lock
+- scrot
---
+++ dogelock/lock.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+img=/tmp/screenlock.png
+doge=$HOME/dotfiles/dogelock/doge.png
+
+scrot $img
+convert $img -level 0%,100% \
+	-gamma 0.16,0.15,0.15 \
+	-filter Gaussian -resize 20% -define filter:sigma=0.6 -resize 500% \
+	-font "Noto-Sans" -pointsize 40 -gravity center \
+	$doge -gravity center -composite \
+	$img
+
+i3lock -f -i $img
+#i3lock -f -u -i $img
