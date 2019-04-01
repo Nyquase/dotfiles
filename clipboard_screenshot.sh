3dc48225059cdb91e08518fd6d4e2482b7cfb771
AdrienR <adrien.rouhete@caregame.com>
2019-04-01 19:00:31 +0200

    Now notifies on error

--- clipboard_screenshot.sh
+++ clipboard_screenshot.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
-#
 # Simple screenshot-to-clipboard script using maim and xclip
-notify-send "Select area to screenshot" &&
-maim -s --format=png /dev/stdout | xclip -selection clipboard -t image/png -i &&
-notify-send "Screenshot has been copied to clipboard"
+
+notify-send "Select area to screenshot"
+
+maim -s --format=png /dev/stdout | xclip -selection clipboard -t image/png -i
+
+if (( "${PIPESTATUS[0]}" == 0 )); then
+  notify-send "Screenshot has been copied to clipboard"
+else
+  notify-send -i dialog-warning "Selection was cancelled by keystroke or right-click."
+fi
