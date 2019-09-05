afe5583769d6e9dbed7cb7e0f8fcd38165ca84b1
Nyquase <adrien.rouhete@epitech.eu>
2019-09-05 15:53:41 +0900

    Fixed lock

--- dogelock/lock.sh
+++ dogelock/lock.sh
@@ -4,15 +4,15 @@ img=/tmp/screenlock.png
 real_script=$(readlink $0)
 doge=$(dirname ${real_script:-$0})/doge.png
 
-echo $doge
-
-scrot $img
+scrot -z -o $img
 convert $img -level 0%,100% \
 	-gamma 0.16,0.15,0.15 \
 	-filter Gaussian -blur 0x8 -resize 20% -define filter:sigma=0.6 -resize 500% \
-	-font "Noto-Sans" -pointsize 40 -gravity center \
 	$doge -gravity center -composite \
 	$img
 
+pkill -u "$USER" -USR1 dunst
 i3lock -f -i $img
+pkill -u "$USER" -USR2 dunst
+
 #i3lock -f -u -i $img
