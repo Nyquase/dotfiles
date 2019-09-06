ae7ba21e3be0d45a6341ecef08d6e10a374d9fe5
Nyquase <adrien.rouhete@epitech.eu>
2019-09-07 00:13:46 +0900

    Added an already existing lockscreen that show doge on blurred wallpaper so the lock is instant, as oposed with the other script that has to convert an image before locking, taking a lot of time

--- dogelock/lock.sh
+++ dogelock/lock.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-img=/tmp/screenlock.png
 real_script=$(readlink $0)
-doge=$(dirname ${real_script:-$0})/doge.png
-
-scrot -z -o $img
-convert $img -level 0%,100% \
-	-gamma 0.16,0.15,0.15 \
-	-filter Gaussian -blur 0x8 -resize 20% -define filter:sigma=0.6 -resize 500% \
-	$doge -gravity center -composite \
-	$img
+img=$(dirname ${real_script:-$0})/lockscreen.png
 
 pkill -u "$USER" -USR1 dunst
 i3lock -f -i $img
 pkill -u "$USER" -USR2 dunst
 
-#i3lock -f -u -i $img
+# i3lock -f -u -i $img
