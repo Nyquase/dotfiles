50062d0ad768ac4415a3852ddcb624c151c4e3a9
Nyquase <adrien.rouhete@epitech.eu>
2022-12-04 02:07:29 +0100

    Don't exit on fail

--- install.sh
+++ install.sh
@@ -11,9 +11,6 @@ function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
 
-# Exit when any command fails
-set -e
-
 # Test if this script was run via the install.sh bin (vs. via curl/wget)
 function is_install_script() {
   [[ "$(basename $0 2>/dev/null)" =~ install.sh ]] || return 1
